d13860abe5f2efe4351ec01110b447a0bbd00319
Julia Sperandio Pereira1 <juliasperandio@ibm.com>
2022-01-25 11:31:40 -0300

    feat: further links

--- test/entities/lecture.spec.ts
+++ test/entities/lecture.spec.ts
@@ -1,4 +1,5 @@
 import { Lecture } from "../../src/entities"
+import { Link } from "../../src/entities/link"
 import { Material } from "../../src/entities/material"
 import { Pdf } from "../../src/entities/pdf"
 
@@ -18,6 +19,14 @@ describe('Lecture', () => {
         lecture.remove(branchingPdf)
 
         expect(lecture.includes(branchingPdf)).toBeFalsy
+    })
+
+    it('should be able to add further links to lectures', () => {
+        const lecture: Lecture = new Lecture('Branching', 'https://youtube.com/1234')
+        const branchingLink: Material = new Link('Branching', 'http://page.com/branching.html')
+        lecture.add(branchingLink)
+
+        expect(lecture.includes(branchingLink)).toBeTruthy
     })
 
 })
